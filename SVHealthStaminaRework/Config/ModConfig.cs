b51236b50174e5c3327363e86886d9c5a42357b4
Michael Cox <michaelwcox07@gmail.com>
2024-01-07 15:19:05 -0600

    Added configurations
    This commit should also be signed now.

---
+++ SVHealthStaminaRework/Config/ModConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SVHealthStaminaRework.Config.ConfigOptions;
+
+namespace SVHealthStaminaRework.Config
+{
+    class ModConfig
+    {
+        public HealthConfig Health { get; set; } = new HealthConfig();
+        public StaminaConfig Stamina { get; set; } = new StaminaConfig();
+        public bool IgnorePause { get; set; } = false;
+    }
+}
